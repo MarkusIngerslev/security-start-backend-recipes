1d5b6810abdb817cd5ac5c695b1a679264009a4d
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-05 20:01:06 +0100

    The Recipe Controller

---
+++ src/main/java/dat3/recipe/api/RecipeController.java
@@ -0,0 +1,16 @@
+package dat3.recipe.api;
+
+import dat3.recipe.service.RecipeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipeController {
+
+    private RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+}
