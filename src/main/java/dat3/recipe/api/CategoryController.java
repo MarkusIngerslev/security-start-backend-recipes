ca87ce526adab37813ee858ec6b545df177bc272
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-06 14:30:02 +0100

    Tilføjet nye route til at kunne hente enkelt category ud fra id

--- src/main/java/dat3/recipe/api/CategoryController.java
+++ src/main/java/dat3/recipe/api/CategoryController.java
@@ -1,10 +1,9 @@
 package dat3.recipe.api;
 
+import dat3.recipe.dto.CategoryDto;
 import dat3.recipe.entity.Category;
 import dat3.recipe.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,14 @@ public class CategoryController {
     public List<String> getAllCategories() {
         return categoryService.getAllCategories();
     }
+
+    @GetMapping(path = "/{id}")
+    public CategoryDto getCategoryById(@PathVariable int id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    /*@PostMapping
+    public
+*/
 }
 
--- src/main/java/dat3/recipe/service/CategoryService.java
+++ src/main/java/dat3/recipe/service/CategoryService.java
@@ -1,8 +1,11 @@
 package dat3.recipe.service;
 
+import dat3.recipe.dto.CategoryDto;
 import dat3.recipe.entity.Category;
 import dat3.recipe.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,6 +22,11 @@ public class CategoryService {
         List<Category> categories =  categoryRepository.findAll();
         //Convert from list of Categories to DTO-type, list of Strings
         return categories.stream().map((c)->new String(c.getName())).toList();
+    }
+
+    public CategoryDto getCategoryById(int id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        return new CategoryDto(category,false);
     }
 }
 
